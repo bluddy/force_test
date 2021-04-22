a588c806daf59984804598090db7279b393754ce
Yotam Barnoy <yotambarnoy@gmail.com>
2021-04-22 18:03:12 -0400

    fixes. Now works

--- force_test.ino
+++ force_test.ino
@@ -3,6 +3,7 @@
 int sensor_pin = A0;
 int value = 0;
 unsigned long time = 0;
+int do_send = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -10,8 +11,15 @@ void setup() {
 }
 
 void loop() {
-  value = analogRead(sensor_pin);
-  time = millis()
-  Serial.write(time);
-  Serial.write(value);
+  if (Serial.available() > 0) {
+    do_send = Serial.read();
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+  if (do_send != 0) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    value = analogRead(sensor_pin);
+    time = millis();
+    Serial.write((byte *)&time, sizeof(time));
+    Serial.write((byte *)&value, sizeof(value));
+  }
 }
--- force_test.py
+++ force_test.py
@@ -4,31 +4,65 @@ import os.path as path
 import cv2
 import numpy as np
 
+import time
 import serial
-import pandas as pd
+#import pandas as pd
 import struct
+import csv
 
 cur_dir = path.dirname(path.abspath(__file__))
 
-def main(args):
-    
-    d = {'time': [], 'voltage': []}
+def get_portname():
+    import platform
+    if platform.system() == 'Linux':
+        import glob
+        # Note: if you get a permission error, you might need to add yourself to the dialout group:
+        # sudo adduser name dialout
+        # reboot for it to take effect
+        # TODO: switch to more robust list_ports
+        port_l = glob.glob('/dev/ttyUSB*')
+        if len(port_l) == 0:
+            port_l = glob.glob('/dev/ttyACM*')
+            if len(port_l) == 0:
+                print("No serial port found for Arduino")
+                sys.exit(1)
+        port = port_l[0]
+    else:
+        port = 'COM4'
+    return port
 
-    ser = serial.Serial(port, 9600)
-    print("sleeping for 2 seconds after serial port has opened")
-    time.sleep(2)
+def main(args):
+
+    time_l = []
+    voltage_l = []
+
+    ser = serial.Serial(get_portname(), 9600)
+    print("Connected!")
+    time.sleep(1)
+
+    ser.write('\x01'.encode()) # start process
+    print("Recording voltages")
 
     try:
         while True:
             bytes = ser.read(4 + 2) # 32-bit unsigned int + 16-bit int
-            (time, voltage) = struct.unpack('Ih', bytes)
-            d['time'].append(time)
-            d['voltage'].append(voltage)
+            (t, v) = struct.unpack('Ih', bytes)
+            time_l.append(t)
+            voltage_l.append(v)
     except:
         raise
     finally:
-        df = pd.DataFrame(data=d)
-        df.to_csv('args.out')
+        print(f"Dumping to args.out")
+
+        ser.write('\x00'.encode()) # stop process
+        with open(args.out, 'w') as csv_f:
+            writer = csv.writer(csv_f, delimiter=',')
+            for i, t in enumerate(time_l):
+                print(i, t)
+                writer.writerow((t, voltage_l[i]))
+
+        #df = pd.DataFrame(data=d)
+        #df.to_csv(args.out)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
